f06c5c341accd8c4d0958cfbe06fc5f339b39fce
IdhulRahman <idhulrahim@gmail.com>
2024-09-11 12:51:32 +0800

    Update query_engine.py

--- config/query_engine.py
+++ config/query_engine.py
@@ -1,6 +1,11 @@
+from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
+
 def setup_query_engine(index, llm):
+    rerank = FlagEmbeddingReranker(model="mixedbread-ai/mxbai-rerank-large-v1", top_n=3)
+    
     return index.as_query_engine(
-        llm=llm, 
+        llm=llm,
+        node_postprocessors=[rerank], 
         vector_store_query_mode="mmr", 
         vector_store_kwargs={"mmr_threshold": 0.2},
         streaming=True
